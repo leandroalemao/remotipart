2a49801d6161f5dc2fe848d713fba4832d6fa350
Mark Hunt <mhunt@diligent.com>
2019-08-02 11:53:22 -0400

    refactor for deprecations
    update the alias deprecation

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -5,14 +5,34 @@ module Remotipart
     include ERB::Util
 
     def self.included(base)
-      base.class_eval do
-        alias_method_chain :render, :remotipart
+      if respond_to? :prepend
+        base.prepend Prependable
+      else
+        base.include Includable
+        # Do not use alias_method_chain for compatibility
+        alias render_without_remotipart render
+        alias render render_with_remotipart
       end
     end
 
-    def render_with_remotipart *args
-      render_without_remotipart *args
-      response_body
+    module Prependable
+      def render(*args, &block)
+        treat_render_for_remotipart super(*args, &block)
+      end
+    end
+
+    module Includable
+      def render_with_remotipart(*args, &block)
+        treat_render_for_remotipart render_without_remotipart(*args, &block)
+      end
+    end
+
+    def treat_render_for_remotipart(render_return_value)
+      if remotipart_submitted?
+        response_body
+      else
+        render_return_value
+      end
     end
   end
 end
